a120d61313aa55ae30302aded7e5c0a8a1dfb5c3
Matias Baldanza <matiasbaldanza@gmail.com>
2024-10-07 08:09:27 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1 className="mx-auto text-3xl font-bold text-red-500">Hola Mundo</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
