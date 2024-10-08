ecce8cdb43c703019d9d1e661f59882f9ca9cb93
Matias Baldanza <matiasbaldanza@gmail.com>
2024-10-08 09:24:35 -0300

    feat (qr-code-card): add compact size

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,19 @@ import QrCodeCard from './components/qr-code-card'
 function App() {
   return (
     <>
-      <div className="w-full h-screen p-4 m-0 bg-slate-300">
+      <div className="flex flex-col items-center w-full h-screen gap-5 p-4 m-0 bg-slate-300">
+        <QrCodeCard
+          compact
+          imageSrc="/01-qr-code/image-qr-code.png"
+          altText="QR Code"
+          href="https://www.frontendmentor.io/"
+          title="Improve your front-end skills by building projects"
+          description="Scan the QR code to visit Front-End Mentor and take your coding skills to the next level"
+          className={`
+            hover:shadow-lg hover:-translate-y-0.5 transition-all duration-150
+            active:shadow-none active:translate-y-0
+          `}
+        />
         <QrCodeCard
           imageSrc="/01-qr-code/image-qr-code.png"
           altText="QR Code"
--- src/components/qr-code-card.tsx
+++ src/components/qr-code-card.tsx
@@ -7,18 +7,24 @@ type QRCodeCardProps = {
   href?: string
   title: string
   description: string
+  compact?: boolean
   className?: string
 }
 
 export default function QRCodeCard(
-  { imageSrc, altText, href, title, description, className }: QRCodeCardProps
+  { imageSrc, altText, href, title, description, compact, className }: QRCodeCardProps
 ) {
-  const baseStyle = "flex flex-col items-center gap-6 p-5 mx-auto bg-white rounded-2xl max-w-[354px]"
+  const baseStyle = cn(
+    "flex items-center gap-6 p-5 mx-auto bg-white rounded-2xl max-w-[354px]",
+    compact
+      ? "flex flex-row gap-4 p-5"
+      : "flex flex-col gap-6 p-5"
+  )
 
 
   const qrImage = (
     <img
-      className="rounded-xl"
+      className={cn("rounded-xl", compact ? "max-h-40" : null)}
       src={imageSrc} alt={altText} />
   )
   return (
@@ -32,10 +38,15 @@ export default function QRCodeCard(
           qrImage
         )
       }
-      <div className="flex flex-col items-center gap-4 pb-6 mx-5">
-        <h2 className="text-2xl font-[700] text-slate-900 font-outfit text-center">{title}</h2>
-        <p className="text-base font-[400] text-slate-500 font-outfit text-center">{description}</p>
+      <div className={cn("flex flex-col items-center gap-4", compact ? "mx-2" : "mx-5")}>
+        <h2 className={cn(
+          "font-[700] text-slate-900 font-outfit",
+          compact ? "text-xl text-left leading-tight font-[400]" : "text-2xl text-center")}>{title}</h2>
+        {compact
+          ? null
+          : <p className="text-base font-[400] text-slate-500 font-outfit text-center pb-6 ">{description}</p>
+        }
       </div>
-    </Card>
+    </Card >
   )
 }
